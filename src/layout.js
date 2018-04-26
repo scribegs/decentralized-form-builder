75656b11767511014708df35ba90337760c12b8b
Timothy Warren <tim@scribe.gs>
2018-04-25 21:22:00 -0500

    Added height style to the layout component

--- src/layout.js
+++ src/layout.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import { Navbar } from './components/navbar'
+import React, { Component } from "react";
+import { Navbar } from "./components/navbar";
 
 class Layout extends Component {
-  render () {
+  render() {
     return (
-      <div>
+      <div style={{ height: "90%", paddingBottom: 10 }}>
         <Navbar />
         <div className="container" style={styles.container}>
           {this.props.children}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -18,6 +18,6 @@ const styles = {
   container: {
     marginTop: 40
   }
-}
+};
 
-export default Layout
+export default Layout;
